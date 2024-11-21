e90d0cb77d037a4b52c802af0c19c6657563a522
Rodrigo Seliprin <rseliprin@gmail.com>
2024-11-21 08:38:58 -0300

    finalização do projeto

--- src/app/components/cotacao-atual/cotacao-atual.component.ts
+++ src/app/components/cotacao-atual/cotacao-atual.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {CurrencyPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-cotacao-atual',
   standalone: true,
-  imports: [],
+    imports: [
+        CurrencyPipe,
+        NgForOf
+    ],
   templateUrl: './cotacao-atual.component.html',
   styleUrl: './cotacao-atual.component.css'
 })
